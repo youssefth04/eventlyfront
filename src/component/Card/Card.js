012ac04e9e9f3a23d899ef1888b6211c8bf64e19
youssef tahiri <ucef.th@gmail.com>
2024-06-20 13:50:52 +0100

    changes

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Card extends Component {
   render() {
-    const { name, date, place, price,img } = this.props;
-    
+    const { name, date, place, price, img, onAddToBasket } = this.props;
+
     return (
       <div>
         <div className="container">
@@ -14,12 +14,13 @@ class Card extends Component {
               <p className="card-info">Date: {date}</p>
               <p className="card-info">Place: {place}</p>
               <p className="card-info">Price: {price} MAD</p>
-              <a href="#" className="buy-btn" >Add to basket</a>
+              <button className="buy-btn" onClick={onAddToBasket}>
+                Add to basket
+              </button>
             </div>
           </div>
         </div>
       </div>
-      
     );
   }
 }
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,13 +3,13 @@ import Navbar from "../component/Navbar/Navbar";
 import Footer from "../component/Footer/Footer";
 import Card from "../component/Card/Card";
 import Search from "../component/Search/Search";
-import "./pages.css"; 
+import "./pages.css";
 
 class DashboardPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortBy: 'name', 
+      sortBy: "name",
       cards: [
         { name: "MERTO BOOMIN", date: "22/06/2024", place: "Rabat", price: 150, img: "/img/metro.png" },
         { name: "SCH", date: "24/06/2024", place: "Rabat", price: 250, img: "/img/schh.png" },
@@ -17,7 +17,8 @@ class DashboardPage extends Component {
         { name: "ELGRANDETOTO", date: "26/06/2024", place: "Rabat", price: 1, img: "/img/toto.jpeg" },
         { name: "CANTRAL CEE", date: "27/06/2024", place: "Rabat", price: 300, img: "/img/cee.png" },
         { name: "LARTISTE", date: "28/06/2024", place: "Rabat", price: 150, img: "/img/lartiste-.jpg" }
-      ]
+      ],
+      basketQuantity: 0
     };
   }
 
@@ -25,14 +26,20 @@ class DashboardPage extends Component {
     this.setState({ sortBy: event.target.value });
   };
 
+  handleAddToBasket = () => {
+    this.setState((prevState) => ({
+      basketQuantity: prevState.basketQuantity + 1
+    }));
+  };
+
   render() {
-    const { sortBy, cards } = this.state;
+    const { sortBy, cards, basketQuantity } = this.state;
     const sortedCards = [...cards].sort((a, b) => {
-      if (sortBy === 'name') {
+      if (sortBy === "name") {
         return a.name.localeCompare(b.name);
-      } else if (sortBy === 'date') {
+      } else if (sortBy === "date") {
         return new Date(a.date) - new Date(b.date);
-      } else if (sortBy === 'price') {
+      } else if (sortBy === "price") {
         return a.price - b.price;
       }
       return 0;
@@ -40,19 +47,25 @@ class DashboardPage extends Component {
 
     return (
       <div>
-        <Navbar f="none" g="none" quantity={0} svg={<svg xmlns='http://www.w3.org/2000/svg' width='35' height='35' fill='currentColor' className='bi bi-handbag-fill' viewBox='0 0 16 16'>
+        <Navbar f="none" g="none" quantity={basketQuantity}  svg={<svg xmlns='http://www.w3.org/2000/svg' width='35' height='35' fill='currentColor' className='bi bi-handbag-fill' viewBox='0 0 16 16'>
           <path d='M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2M5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0z'/>
         </svg>} />
         <Search />
         <div className="sort-by">
           <span>Sort By:</span>
-          <label><input type="radio" name="sort" value="name" checked={sortBy === 'name'} onChange={this.handleSortChange} /> Name</label>
-          <label><input type="radio" name="sort" value="date" checked={sortBy === 'date'} onChange={this.handleSortChange} /> Date</label>
-          <label><input type="radio" name="sort" value="price" checked={sortBy === 'price'} onChange={this.handleSortChange} /> Price</label>
+          <label>
+            <input type="radio" name="sort" value="name" checked={sortBy === "name"} onChange={this.handleSortChange} /> Name
+          </label>
+          <label>
+            <input type="radio" name="sort" value="date" checked={sortBy === "date"} onChange={this.handleSortChange} /> Date
+          </label>
+          <label>
+            <input type="radio" name="sort" value="price" checked={sortBy === "price"} onChange={this.handleSortChange} /> Price
+          </label>
         </div>
         <div className="container">
           {sortedCards.map((card, index) => (
-            <Card key={index} name={card.name} date={card.date} place={card.place} price={card.price} img={card.img} />
+            <Card key={index} name={card.name} date={card.date} place={card.place} price={card.price} img={card.img} onAddToBasket={this.handleAddToBasket} />
           ))}
         </div>
         <Footer />
